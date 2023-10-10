63a20007ba38a52306f6b1b4d7548c364d40cb57
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2023-10-10 18:49:26 +0330

    Use dic to unpack args

--- conftests/browser_instance_handler.py
+++ conftests/browser_instance_handler.py
@@ -15,12 +15,13 @@ def browser(config):
         'edge': webdriver.Edge,
     }
     browsers_headless = {'chrome_h': 'headless chrome'}
+    browser_args = {}
     if browser_name == browsers_headless['chrome_h']:
+        browser_name = 'chrome'
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
-        browser_instance = webdriver.Chrome(options=options)
-    else:
-        browser_instance = browsers[browser_name]()
+        browser_args['options'] = options
+    browser_instance = browsers[browser_name](**browser_args)
     if implicit_wait_state:
         browser_instance.implicitly_wait(implicit_wait)
     yield browser_instance
