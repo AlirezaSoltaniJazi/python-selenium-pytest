449e5839d4a67ba9faeef1f9d80e9156fa1e6f0b
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2023-10-10 18:49:43 +0330

    Fix exception type

--- conftests/config_data_handler.py
+++ conftests/config_data_handler.py
@@ -20,11 +20,11 @@ def config():
     if browser_name not in ['headless chrome', 'firefox', 'chrome', 'safari', 'edge']:
         raise TypeError(f'Browser {browser_name} is not supported!')
     if not isinstance(implicit_wait_state, bool):
-        raise ValueError(
+        raise TypeError(
             f'The implicit wait state {implicit_wait_state} must be a boolean'
         )
     if not isinstance(implicit_wait, int):
-        raise ValueError(f'The implicit wait time {implicit_wait} must be an integer')
+        raise TypeError(f'The implicit wait time {implicit_wait} must be an integer')
     if implicit_wait < 0:
         raise ValueError(f'The implicit wait time must be greater than {implicit_wait}')
     return config_data
